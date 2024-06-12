e3cf94cdb2cfa9d284e572614dc4868513e83b76
akash644 <82668579+akash644@users.noreply.github.com>
2024-06-12 12:37:38 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { FormEvent, useState } from 'react';
+import { setToken, setUser } from '@/redux/auth/auth.slice';
+import useAuthSession from '../hooks/useAuthSession';
+import { useAppDispatch } from '@/redux/store';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
+
+const HomePage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useAppDispatch();
+  const user = useAuthSession();
+
+  const handleLogin = async () => {
+    try {
+
+      if (username.length < 1 || password.length < 6 ) {
+        toast.error('Enter valid credentials');
+        return;
+      }
+  
+      const response = await axios.post('/api/auth/login', {
+        username: username,
+        password: password,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+
+      const data = await response.data
+
+      dispatch(setToken(data.token));
+      dispatch(setUser(data.user));
+
+      setUsername('');
+      setPassword('');
+
+      toast.success(data.message);
+
+    } catch (error: any) {
+      toast.error(error.response.data.message ? error.response.data.message : error.message);
+    }
+  }
+
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+        {user ? (
+          <div>
+            <h2 className="text-xl font-bold">Welcome, {user.username}</h2>
+          </div>
+        ) : (
+          <div>
+            <h2 className="text-2xl font-bold text-center">Login</h2>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+              className="w-full px-4 py-2 mt-4 border text-black rounded-md"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              minLength={6}
+              placeholder="Password"
+              className="w-full px-4 py-2 mt-4 border text-black rounded-md"
+            />
+            <button
+              onClick={handleLogin}
+              className="w-full px-4 py-2 mt-6 font-bold text-white bg-blue-500 rounded-md"
+            >
+              Login
+            </button>
+          </div>
+        )}
+        
+      </div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default HomePage;
